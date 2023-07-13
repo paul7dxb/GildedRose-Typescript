9183e3157987fd513db0c55603aa2815d63e27fe
unknown <paul7markets@hotmail.com>
2023-07-13 12:35:57 +0100

    Bound quality at end of loop

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,36 +19,28 @@ export class GildedRose {
 
 	updateQuality() {
 		for (let item of this.items) {
-
 			if (item.name === "Sulfuras, Hand of Ragnaros") {
-				continue;
+				continue
 			}
 
 			const sellIn = item.sellIn
+
+			if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+			}
 
 			if (
 				item.name != "Aged Brie" &&
 				item.name != "Backstage passes to a TAFKAL80ETC concert"
 			) {
-				if (item.quality > 0) {
-						item.quality = item.quality - 1
-				}
+				item.quality = item.quality - 1
 			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1
-					if (
-						item.name == "Backstage passes to a TAFKAL80ETC concert"
-					) {
-						if (sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1
-							}
-						}
-						if (sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1
-							}
-						}
+				item.quality = item.quality + 1
+				if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+					if (sellIn < 11) {
+						item.quality = item.quality + 1
+					}
+					if (sellIn < 6) {
+						item.quality = item.quality + 1
 					}
 				}
 			}
@@ -58,23 +50,19 @@ export class GildedRose {
 					if (
 						item.name != "Backstage passes to a TAFKAL80ETC concert"
 					) {
-						if (item.quality > 0) {
-								item.quality = item.quality - 1
-							}
+						item.quality = item.quality - 1
 					} else {
 						item.quality = 0
 					}
 				} else {
-					if (item.quality < 50) {
-						// item.quality = item.quality + 1
-						this.increaseQuality(item, 1)
-					}
+					// item.quality = item.quality + 1
+					this.increaseQuality(item, 1)
 				}
 			}
 
+			this.containQualityValues(item)
 			//  Reduce sellIn Date
-				item.sellIn--
-
+			item.sellIn--
 		}
 
 		return this.items
@@ -85,5 +73,13 @@ export class GildedRose {
 	}
 	increaseQuality(item, value) {
 		item.quality = item.quality + value
+	}
+	containQualityValues(item) {
+		if (item.quality < 0) {
+			item.quality = 0
+		}
+		if (item.quality > 50) {
+			item.quality = 50
+		}
 	}
 }
