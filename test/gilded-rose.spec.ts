926f749abfd5f9619194e54a9953729f4b98702a
unknown <paul7markets@hotmail.com>
2023-07-13 12:26:27 +0100

    Added tests for pass increase

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -32,6 +32,23 @@ describe("Gilded Rose", function () {
         expect(items[0].quality).to.equal(0)
 	})
 
+	it("backstage pass value goes up by one when days > 10", function () {
+		const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 25)])
+		const items = gildedRose.updateQuality()
+        expect(items[0].quality).to.equal(26)
+	})
+	it("backstage pass value goes up by 2 when 10 >= days > 5 ", function () {
+		const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25)])
+		const items = gildedRose.updateQuality()
+        expect(items[0].quality).to.equal(27)
+	})
+
+	it("backstage pass value goes up by 3 when 5 >= days > 0 ", function () {
+		const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 25)])
+		const items = gildedRose.updateQuality()
+        expect(items[0].quality).to.equal(28)
+	})
+
 
 
 
