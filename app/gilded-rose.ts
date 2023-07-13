0c65f9806ee422a8e3f1f3388899449518efa67f
unknown <paul7markets@hotmail.com>
2023-07-13 12:21:38 +0100

    sulfuras check at start of loop

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,87 +1,89 @@
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
+	name: string
+	sellIn: number
+	quality: number
 
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+	constructor(name, sellIn, quality) {
+		this.name = name
+		this.sellIn = sellIn
+		this.quality = quality
+	}
 }
 
 export class GildedRose {
-    items: Array<Item>;
+	items: Array<Item>
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
+	constructor(items = [] as Array<Item>) {
+		this.items = items
+	}
 
-    updateQuality() {
-        
-        for (let item of this.items) {
+	updateQuality() {
+		for (let item of this.items) {
 
-            const sellIn = item.sellIn;
+			if (item.name === "Sulfuras, Hand of Ragnaros") {
+				continue;
+			}
 
-            if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.quality > 0) {
-                    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                        item.quality = item.quality - 1
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                        if (sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
+			const sellIn = item.sellIn
 
-            if (sellIn <= 0) {
-                if (item.name != 'Aged Brie') {
-                    if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.quality > 0) {
-                            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        // item.quality = item.quality + 1
-                        this.increaseQuality(item, 1);
-                    }
-                }
-            }
+			if (
+				item.name != "Aged Brie" &&
+				item.name != "Backstage passes to a TAFKAL80ETC concert"
+			) {
+				if (item.quality > 0) {
+						item.quality = item.quality - 1
+				}
+			} else {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1
+					if (
+						item.name == "Backstage passes to a TAFKAL80ETC concert"
+					) {
+						if (sellIn < 11) {
+							if (item.quality < 50) {
+								item.quality = item.quality + 1
+							}
+						}
+						if (sellIn < 6) {
+							if (item.quality < 50) {
+								item.quality = item.quality + 1
+							}
+						}
+					}
+				}
+			}
 
-            //  Reduce sellIn Date
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.sellIn -- ;
-            }
+			if (sellIn <= 0) {
+				if (item.name != "Aged Brie") {
+					if (
+						item.name != "Backstage passes to a TAFKAL80ETC concert"
+					) {
+						if (item.quality > 0) {
+								item.quality = item.quality - 1
+							}
+					} else {
+						item.quality = 0
+					}
+				} else {
+					if (item.quality < 50) {
+						// item.quality = item.quality + 1
+						this.increaseQuality(item, 1)
+					}
+				}
+			}
 
-        }
+			//  Reduce sellIn Date
+				item.sellIn--
 
+		}
 
-        return this.items;
-    }
+		return this.items
+	}
 
-    decreaseQuality(item, value ){
-        item.quality = item.quality - value
-    }
-    increaseQuality(item, value ){
-        item.quality = item.quality + value
-    }
-
+	decreaseQuality(item, value) {
+		item.quality = item.quality - value
+	}
+	increaseQuality(item, value) {
+		item.quality = item.quality + value
+	}
 }
