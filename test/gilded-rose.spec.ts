af5e74663a8a87a32f956f13e69c6114eccf5ebb
unknown <paul7markets@hotmail.com>
2023-07-13 11:46:42 +0100

    test for sellIn change sulfuras and not sulfuras

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -8,4 +8,17 @@ describe("Gilded Rose", function () {
 		expect(items[0].name).to.equal("foo")
 	})
 
+	it("decrease sellIn by 1", function () {
+		const gildedRose = new GildedRose([new Item("foo", 5, 0)])
+		const items = gildedRose.updateQuality()
+		expect(items[0].sellIn).to.equal(4)
+	})
+	it("don't decrease sellIn by 1 for sulfuras", function () {
+		const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 5, 0)])
+		const items = gildedRose.updateQuality()
+        expect(items[0].sellIn).to.equal(5)
+	})
+
+
+
 })
