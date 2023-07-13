427c673ceac3654c6bfcdd43db2f44b0ddc62dca
unknown <paul7markets@hotmail.com>
2023-07-13 14:11:04 +0100

    quality change rate added for sellIn items

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -14,58 +14,52 @@ export class GildedRose {
 	items: Array<Item>
 
 	constructor(items = [] as Array<Item>) {
-		this.items = items;
+		this.items = items
 	}
 
 	updateQuality() {
-		const changeRate = 1;
-
 		for (let item of this.items) {
 			// Sulfuras. They don't expire
 			if (item.name === "Sulfuras, Hand of Ragnaros") {
-				continue;
+				continue
 			}
 
 			// All other products than sulfuras get older
-			item.sellIn--;
+			item.sellIn--
 
 			// Backstage passes
 			if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
 				if (item.sellIn < 0) {
-					item.quality = 0;
+					item.quality = 0
 				} else {
 					const passIncrease =
 						item.sellIn < 5 ? 3 : item.sellIn < 10 ? 2 : 1
-					this.increaseQuality(item, passIncrease);
+					this.increaseQuality(item, passIncrease)
 				}
-				continue;
+				continue
 			}
+
+			// Items affected by Expiry
+			const qualityChangeRate = item.sellIn < 0 ? 2 : 1
 
 			// Aged Brie
 			if (item.name === "Aged Brie") {
-				if (item.sellIn < 0) {
-					this.increaseQuality(item, 2);
-				} else {
-					this.increaseQuality(item, 1);
-				}
-				continue;
+				this.increaseQuality(item, qualityChangeRate)
+				continue
 			}
 
-            // Other items
-			if (item.sellIn < 0) {
-				this.decreaseQuality(item, 2);
-			} else {
-				this.decreaseQuality(item, 1);
-			}
+			// Other items
+			this.decreaseQuality(item, qualityChangeRate)
+            
 		}
 
-		return this.items;
+		return this.items
 	}
 
 	decreaseQuality(item, value) {
-		item.quality = item.quality - value < 0 ? 0 : item.quality - value;
+		item.quality = item.quality - value < 0 ? 0 : item.quality - value
 	}
 	increaseQuality(item, value) {
-		item.quality = item.quality + value > 50 ? 50 : item.quality + value;
+		item.quality = item.quality + value > 50 ? 50 : item.quality + value
 	}
 }
